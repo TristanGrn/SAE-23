c66321531a47275bca4d262c85c8a49f53238864
TristanGrn <77834175+TristanGrn@users.noreply.github.com>
2023-05-12 16:09:15 +0200

    Ajout de Index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+// include...
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>SAE 23 - Granjeon Keroulas</title>
+</head>
+<body>
+	<header>
+		<h1>WEB2 TD56 TP45 PHP Une Application BDD Granjeon</h1>
+	</header>
+
+	<footer>
+			<p>Pied de la page <!-- A COMPLETER --></p>
+		</footer>
+</body>
+</html>
