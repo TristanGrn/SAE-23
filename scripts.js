3989ae73fbdf835007249f0dcf9c707ba1edd993
TristanGrn <77834175+TristanGrn@users.noreply.github.com>
2023-06-11 15:00:03 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function valideCaptcha() {
-    var response = grecaptcha.getResponse();
-    if (response.length === 0) {
-        alert("Veuillez valider le Captcha");
-        return false;
-    } else {
-        return true;
-    }
-}
